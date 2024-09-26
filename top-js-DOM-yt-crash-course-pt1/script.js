63340bb18d3f4cdf07954234e745863212afe703
Roan <17789917+RoanOpperman@users.noreply.github.com>
2024-09-26 15:08:21 -0600

    Layout the plan for the list creator component

--- top-js-DOM-yt-crash-course-pt1/script.js
+++ top-js-DOM-yt-crash-course-pt1/script.js
@@ -1,21 +1,29 @@
 `use strict`;
 console.log("index.html & script.js are connected");
-//select the add item button
-//?is it a submit or a regular button: according to the HTML a regular button.
-const buttonToAddListItem = document.getElementById("btn-add-list-item");
-const dltBtn = document.querySelectorAll(".delete-btn");
-//select ul element
-//Let delete button remove list items
-const addListItem = function () {
-  //create li element with input and delete button
-  const containerListItems = document.querySelector("#list-container");
-  const newListItem = document.createElement("li");
-  newListItem.textContent = document.querySelector("#item-input").value;
-  //add delete button
-  const deleteBtn = document.createElement("button");
-  deleteBtn.className = "delete-btn";
-  deleteBtn.textContent = "X";
-  newListItem.appendChild(deleteBtn);
-  containerListItems.appendChild(newListItem);
-};
-buttonToAddListItem.addEventListener("mouseup", addListItem);
+//Select & Store input element value node,
+//DELETE LIST ITEM FUNCTION
+//select the "li" element and remove it
+//
+//CREATE "li" NODE FUNCTION: parameters = delete item)
+//While creating a "li" node --> append the input variable
+//Create delete "button" node,
+//EXTRA: Recreate delete button from scratch only using JS
+//Attach the [".delete-btn"] class to delete "button",
+//ADD EventListiner and pass the delete item function as argument
+//Append input value node to "li" node (child --> li),
+//Append delete "button" node(child --> li) to "li" node,
+//Return the "li"
+//
+//Select and store "Add list item" "button"
+//To the item button add a addEventListener('mouseup')
+//select the "ul" element (parent node) and append the list function call
+//select the input value node and assign(clear it) it with ""
+//focus on the input node
+//
+//EXISTING LIST ITEMS: Select existing list items to allow removal if desired
+//select the "ul" element
+//create a for loop and use the existing "li" element node-list(.length) in the condition
+//while selecting all the existing list items ([i]), append the existing...
+//delete buttons by selecting them at the same time(querySelector)
+//for the button (mouseup) attach the delete item function
+//call the function
